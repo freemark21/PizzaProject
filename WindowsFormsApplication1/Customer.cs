2876252f85dcf7b12236955b2f0ce5aa3d5218e1
student <student@EH255WD12>
2016-09-27 20:45:58 -0500

    Customer Class

---
+++ WindowsFormsApplication1/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProject
+{
+    class Customer
+    {
+        public string CustPhone;
+        public string CustName;
+        public string CustAddr1;
+        public string CustAddr2;
+        public string CustCity;
+        public string CustState;
+        public string CustZip;
+    }
+}
